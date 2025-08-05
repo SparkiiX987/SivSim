36d56f6a55ccbd2933ad1049f860888e7776e201
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2025-08-06 01:21:30 +0200

    add entitied

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] private EntityStats stats = new EntityStats();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
